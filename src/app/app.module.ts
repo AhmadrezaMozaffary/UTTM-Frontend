b68a93ea1f49937e539f9df1fd5cc21001004799
Ahmadreza <ahmadrezam@systemgroup.net>
2024-05-02 23:20:15 +0330

    add some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { COMPONENTS } from './components';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    COMPONENTS
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, COMPONENTS],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
